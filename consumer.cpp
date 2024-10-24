b22e04eb9a69c7da1459f48e3f32cc8157b36c00
natebooth2026 <nbooth5@kent.edu>
2024-10-24 16:17:01 +0000

    Lock draft 1

--- consumer.cpp
+++ consumer.cpp
@@ -1,18 +1,19 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <iostream>
-#include <cstring>
 #include <unistd.h>
 #include <semaphore.h>
+#include <stdio.h>
+#include <cstring>
 
 struct shmbuf {
-    bool flag[2];
-    int turn;
+    bool lock = false;
     int table[2];
 };
 
-const size_t MEM_SIZE = sizeof(bool[2]) + sizeof(int) + sizeof(int[2]);
+const size_t MEM_SIZE = sizeof(bool) + sizeof(int[2]);
+
+bool test_and_set(bool*);
 
 int main(int argc, char *argv[]){
     int memFD = 0;
@@ -20,20 +21,45 @@ int main(int argc, char *argv[]){
 
     memFD = shm_open(SHM_NAME, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
     if(memFD == -1){
-        std::cerr << "Shared memory creation error" << strerror(errno) << std::endl;
+        fprintf(stderr, "Shared memory creation error");
         return 1;
     }
 
     if(ftruncate(memFD, MEM_SIZE) == -1){
-        std::cerr << "Shared memory truncation error: " << strerror(errno) << std::endl;
+        fprintf(stderr, "Shared memory truncation error: ");
         shm_unlink(SHM_NAME);
         return 1;
     }
 
-    void* ptr = mmap(NULL, MEM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, memFD, 0);
-    if(ptr == MAP_FAILED){
-        std::cerr << "Shared memory mapping error: " << strerror(errno) << std::endl;
+    shmbuf* shared = (shmbuf*)mmap(NULL, MEM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, memFD, 0);
+    if(shared == MAP_FAILED){
+        fprintf(stderr, "Shared memory mapping error: ");
         shm_unlink(SHM_NAME);
         return 1;
     }
+
+    int consumed[2];
+    bool item0 = false;
+    do {
+        while(test_and_set(&shared->lock));
+        if(shared->table[0] != NULL){
+            consumed[0] = shared->table[0];
+            printf("Consumed table item 0: ", consumed[0]);
+            shared->table[0] = NULL;
+            item0 = true;
+        } else if (shared->table[1] != NULL){
+            consumed[1] = shared->table[1];
+            printf("Consumed table item 1: ", consumed[1]);
+            shared->table[1] = NULL;
+        }
+        shared->lock = false;
+        if(item0) {consumed[0] = NULL; item0 = false;}
+        else if(!item0) {consumed[1] = NULL;}
+    } while (true);
+}
+
+bool test_and_set(bool *target){
+    bool retVal = *target;
+    *target = true;
+    return retVal;
 }
--- producer.cpp
+++ producer.cpp
@@ -1,39 +1,67 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <iostream>
-#include <cstring>
 #include <unistd.h>
 #include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <cstring>
 
 struct shmbuf {
-    bool flag[2];
-    int turn;
+    bool lock;
     int table[2];
 };
 
-const size_t MEM_SIZE = sizeof(bool[2]) + sizeof(int) + sizeof(int[2]);
+const size_t MEM_SIZE = sizeof(bool) + sizeof(int[2]);
+
+bool test_and_set(bool*);
 
 int main(int argc, char *argv[]){
+    srand(time(nullptr));
+
     int memFD = 0;
     const char* SHM_NAME = "/table_memory";
 
     memFD = shm_open(SHM_NAME, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
     if(memFD == -1){
-        std::cerr << "Shared memory creation error" << strerror(errno) << std::endl;
+        fprintf(stderr, "Shared memory creation error");
         return 1;
     }
 
     if(ftruncate(memFD, MEM_SIZE) == -1){
-        std::cerr << "Shared memory truncation error: " << strerror(errno) << std::endl;
+        fprintf(stderr, "Shared memory truncation error: ");
         shm_unlink(SHM_NAME);
         return 1;
     }
 
-    void* ptr = mmap(NULL, MEM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, memFD, 0);
-    if(ptr == MAP_FAILED){
-        std::cerr << "Shared memory mapping error: " << strerror(errno) << std::endl;
+    shmbuf* shared = (shmbuf*)mmap(NULL, MEM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, memFD, 0);
+    if(shared == MAP_FAILED){
+        fprintf(stderr, "Shared memory mapping error: ");
         shm_unlink(SHM_NAME);
         return 1;
     }
+
+    int produced[2] = {rand() % 1000, rand() % 1000};
+    bool item0 = false;
+    do {
+        while(test_and_set(&shared->lock));
+        if(shared->table[0] == NULL){
+            shared->table[0] = produced[0];
+            printf("Produced table item 0: ", shared->table[0]);
+            item0 = true;
+        } else if (shared->table[1] == NULL){
+            shared->table[1] = produced[1];
+            printf("Produced table item 1: ", shared->table[0]);
+        }
+        shared->lock = false;
+        if(item0) {produced[0] = rand(); item0 = false;}
+        else if(!item0) produced[1] = rand();
+    } while (true);
+}
+
+bool test_and_set(bool *target){
+    bool retVal = *target;
+    *target = true;
+    return retVal;
 }
