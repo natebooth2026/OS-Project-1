c7d86c12992215f599490595488753b96987d6ac
natebooth2026 <nbooth5@kent.edu>
2024-10-31 14:24:15 +0000

    actual final code

--- consumer.cpp
+++ consumer.cpp
@@ -7,93 +7,101 @@
 #include <semaphore.h>
 #include <pthread.h>
 
+//shared memory structure
 struct shmbuf {
     sem_t s;
-    int table[100];
+    int table[2];
 };
 
-const size_t MEM_SIZE = sizeof(sem_t) + sizeof(int[100]);
+const size_t MEM_SIZE = sizeof(sem_t) + sizeof(int[2]); //size of shared memory
 
+//holds consumption algorithm to be run by thread
 void* consumption(void*);
 
 int main(){
+    //defines and creates shared memory (if not created already)
     int memFD = 0;
     const char* SHM_NAME = "/table_memory";
 
     memFD = shm_open(SHM_NAME, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
-    if(memFD == -1){
+    if(memFD == -1){ //if failure
         fprintf(stderr, "Shared memory creation error");
         return 1;
     }
 
-    if(ftruncate(memFD, MEM_SIZE) == -1){
+    //defines memory size
+    if(ftruncate(memFD, MEM_SIZE) == -1){ //if failed
         fprintf(stderr, "Shared memory truncation error");
         shm_unlink(SHM_NAME);
         return 1;
     }
 
+    //maps memory
     shmbuf* shared = (shmbuf*)mmap(NULL, MEM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, memFD, 0);
-    if(shared == MAP_FAILED){
+    if(shared == MAP_FAILED){ //if failed
         fprintf(stderr, "Shared memory mapping error");
         shm_unlink(SHM_NAME);
         return 1;
     }
 
+    //defines, creates, and runs thread
     pthread_t thread;
-    if(pthread_create(&thread, NULL, consumption, (void*)shared) != 0){
+    if(pthread_create(&thread, NULL, consumption, (void*)shared) != 0){ //f failed
         fprintf(stderr, "Consumption thread creation error\n");
         shm_unlink(SHM_NAME);
         return 1;
     }
     pthread_join(thread, NULL);
 
-    munmap(shared, MEM_SIZE);
-    shm_unlink(SHM_NAME);
+    sem_close(&shared->s); //deallocates resources used on the semaphore
+    munmap(shared, MEM_SIZE); //unmaps the mamory
+    shm_unlink(SHM_NAME); //elimates access to the shared memory
     return 0;
 }
 
+//runs consumption algorithm on the shared table
 void* consumption(void* arg){
-    shmbuf* shared = (shmbuf*)arg;
+    shmbuf* shared = (shmbuf*)arg; //converts void* shared back to shmbuf* shared
 
-    int consumed = 0;
-    int size = 0;
-    bool empty = false;
-    int originalI = 0;
+    int consumed = 0; //holds consumed number
+    int size = 0; //holds size of table
+    bool empty = false; //denotes if taable is empty or not
+    int originalI = 0; //holds at what iteration the table was denoted empty
     
     for(int i = 0; i < 100; ++i){
-        sem_wait(&shared->s);
+        sem_wait(&shared->s); //waits for control
 
-        for(int j = 0; j < 100; ++j){
-            if(shared->table[j] != 0){
-                ++size;
-            }
-        }
+        //size calculation
+        if(shared->table[0] != 0) ++size;
+        if(shared->table[1] != 0) ++size;
 
-        if(size == 0 && !empty) {
-            sem_post(&shared->s);
-            empty = true;
-            originalI = i;
-            --i;
+        //size safety check
+        if(size == 0 && !empty) { //empty but not denoted empty yet
+            sem_post(&shared->s); //passes control to producer
+            empty = true; //denotes table is empty
+            originalI = i; //in case table remains empty next iteration
+            --i; //sends back to current i for next iteration
             continue;
-        } else if (size == 0){
-            sem_post(&shared->s);
-            i = originalI - 1;
+        } else if (size == 0){ //still empty
+            sem_post(&shared->s); //passes control
+            i = originalI - 1; //resets i back to when originally empty
             continue;
-        } else {
-            size = 0;
-            empty = false;
+        } else { //not empty
+            size = 0; //resets size
+            empty = false; //table is not empty
         }
         
-        if(shared->table[i] != 0){
-            consumed = shared->table[i];
+        //takes item out of table if not "null" and makes table item "null", prints item taken out
+        if(shared->table[i % 2] != 0){
+            consumed = shared->table[i % 2];
             printf("Consumed table item %d", i + 1);
             printf(": %d\n", consumed);
-            shared->table[i] = 0;
+            shared->table[i % 2] = 0;
         }
         
     
-        sem_post(&shared->s);
-        consumed = 0;
+        sem_post(&shared->s); //passes control to producer
+        consumed = 0; //erases old value
     }
 
     return NULL;
--- producer.cpp
+++ producer.cpp
@@ -9,105 +9,118 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+//shared memory structure
 struct shmbuf {
     sem_t s;
-    int table[100];
+    int table[2];
 };
 
-const size_t MEM_SIZE = sizeof(sem_t) + sizeof(int[100]);
+const size_t MEM_SIZE = sizeof(sem_t) + sizeof(int[2]); //holds shared memory size
 
+//holds the production algorithm to be called by the thread
 void* production(void*);
 
 int main(){
-    srand(time(nullptr));
+    srand(time(nullptr)); //for the random numbers
 
+    //defines and opens shared memory (if not opened already)
     int memFD = 0;
     const char* SHM_NAME = "/table_memory";
 
     memFD = shm_open(SHM_NAME, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
-    if(memFD == -1){
+    if(memFD == -1){ //if shm_open() failed
         fprintf(stderr, "Shared memory creation error");
         return 1;
     }
 
-    if(ftruncate(memFD, MEM_SIZE) == -1){
+    //defines size of the memory
+    if(ftruncate(memFD, MEM_SIZE) == -1){ //if the sizing fails
         fprintf(stderr, "Shared memory truncation error");
         shm_unlink(SHM_NAME);
         return 1;
     }
 
+    //maps the shared memory to support only the shmbuf structure
     shmbuf* shared = (shmbuf*)mmap(NULL, MEM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, memFD, 0);
-    if(shared == MAP_FAILED){
+    if(shared == MAP_FAILED){ //if mapping failed
         fprintf(stderr, "Shared memory mapping error");
         shm_unlink(SHM_NAME);
         return 1;
     }
 
+    //initalizes shared memory
     std::memset(shared, 0, MEM_SIZE);
 
-    if(sem_init(&shared->s, 1, 1) == -1){
+    //initalizes the semaphore in shared
+    if(sem_init(&shared->s, 1, 1) == -1){ //if failed
         fprintf(stderr, "Semaphore initalization error");
         shm_unlink(SHM_NAME);
         return 1;
     }
 
-    for(int i = 0; i < 100; ++i){
-        shared->table[i] = 0;
-    }
+    //initalizes the table
+    shared->table[0] = 0;
+    shared->table[1] = 0;
 
+    //opens, creates, and runs the thread that runs production()
     pthread_t thread;
-    if(pthread_create(&thread, NULL, production, (void*)shared) != 0){
+    if(pthread_create(&thread, NULL, production, (void*)shared) != 0){ //if thread creation fails
         fprintf(stderr, "Production thread creation error\n");
         shm_unlink(SHM_NAME);
         return 1;
     }
     pthread_join(thread, NULL);
 
-    munmap(shared, MEM_SIZE);
-    shm_unlink(SHM_NAME);
+    sem_close(&shared->s); //deallocates resources used on the semaphore
+    munmap(shared, MEM_SIZE); //unmaps the mamory
+    shm_unlink(SHM_NAME); //elimates access to the shared memory
     return 0;
 }
 
+//runs production algorithm on the shared table
 void* production(void* arg){
-    shmbuf* shared = (shmbuf*)arg;
+    shmbuf* shared = (shmbuf*)arg; //converts void* shared to shmbuf* shared to be used
 
-    int produced[2] = {rand() % 1000 + 1, rand() % 1000 + 1};
-    int size = 0;
-    bool full = false;
-    int originalI = 0;
+    int produced[2] = {rand() % 1000 + 1, rand() % 1000 + 1}; //intializes produced values
+    int size = 0; //holds the amount of full elements in the table
+    bool full = false; //denotes if the table is full
+    int originalI = 0; //holds i value when full becomes true
 
+    //checks size contraints and inserts elements into table if control has been given to the process
     for(int i = 0; i < 100; ++i){
-        sem_wait(&shared->s);
+        sem_wait(&shared->s); //waits for semaphore lock
 
-        for(int j = 0; j < 100; ++j){
-            if(shared->table[j] != 0){
-                ++size;
-            }           
-        }
+        //size calculation
+        if(shared->table[0] != 0) ++size;
+        if(shared->table[1] != 0) ++size;
 
-        if(size >= 2 && !full) {
-            sem_post(&shared->s);
-            size = 0;
-            full = true;
-            originalI = i;
-            --i;
+        //size safety check
+        if(size >= 2 && !full) { //full but full has not been denoted yet
+            sem_post(&shared->s); //passes control to consumer
+            size = 0; //resets size
+            full = true; //denotes table is full now
+            originalI = i; //hold original i value in case it still full during next iteration
+            --i; //sends i back to current i after post-statement
             continue;
-        } else if (size >= 2){
-            sem_post(&shared->s);
-            size = 0;
-            i = originalI - 1;
+        } else if (size >= 2) { //still full
+            sem_post(&shared->s); //passes control
+            size = 0; //resets size
+            i = originalI - 1; //resets i to when the table was first full (- 1 due to post-statement)
             continue;
-        } else {
+        } else { //not full
             full = false;
         }
 
-        if(shared->table[i] == 0){
-            shared->table[i] = produced[i % 2];
+        //inserts into table if element is "empty" (== 0), prints item placed in table
+        if(shared->table[i % 2] == 0){
+            shared->table[i % 2] = produced[i % 2];
             printf("Produced table item %d", i + 1);
-            printf(": %d\n", shared->table[i]);
+            printf(": %d\n", shared->table[i % 2]);
         }
 
-        sem_post(&shared->s);
+        sem_post(&shared->s); //passes control to consumer
+
+        //creates new values to pass to table
         produced[0] = rand() % 1000 + 1;
         produced[1] = rand() % 1000 + 1;
     }
